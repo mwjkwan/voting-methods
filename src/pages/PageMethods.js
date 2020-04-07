eebd7f7a135385d6c522eccc7cc206b2ddf06176
Melissa Kwan <>
2020-04-07 12:16:50 -0700

    Add mock cards

--- src/pages/PageMethods.js
+++ src/pages/PageMethods.js
@@ -2,9 +2,11 @@
 
 import { Component } from "react";
 import { css, jsx } from "@emotion/core";
+import { CardColumns, Card } from "react-bootstrap";
 
 import Description from "../components/Description";
 import Article from "../components/Article";
+const methods = require("../assets/data/methods.json");
 
 const story = <div>The different methods we plan to cover.</div>;
 
@@ -16,16 +18,32 @@ const style = css`
   div {
     font-size: 18px !important;
   }
+  .card-columns {
+    margin: 1em;
+    max-width: 90vw;
+  }
 `;
 
 export default class PageMethods extends Component {
   render() {
     return (
-      <Article {...content}>
-        <div css={style}>
-          <Description>{story}</Description>
-        </div>
-      </Article>
+      <div css={style}>
+        <Article {...content}>
+            <Description>{story}</Description>
+        </Article>
+        <CardColumns>
+          {methods.map(method =>
+            <Card>
+              <Card.Body>
+                <Card.Title>{method.name}</Card.Title>
+                <Card.Text>
+                  {method.summary}
+                </Card.Text>
+            </Card.Body>
+            </Card>
+          )}
+      </CardColumns>
+      </div>
     );
   }
 }
