0f62221014bb77b8528e12a405001dd4c7cac629
Melissa Kwan <>
2020-04-07 22:01:25 -0700

    Add toggle feature to methods

--- src/pages/PageMethods.js
+++ src/pages/PageMethods.js
@@ -2,7 +2,7 @@
 
 import { Component } from "react";
 import { css, jsx } from "@emotion/core";
-import { CardColumns } from "react-bootstrap";
+import { CardColumns, Button, ButtonGroup } from "react-bootstrap";
 
 import Description from "../components/Description";
 import Article from "../components/Article";
@@ -23,17 +23,50 @@ const style = css`
   .card-text {
     margin-top: 0.3em;
   }
+  .table {
+    margin: 1em;
+    max-width: 90vw;
+  }
 `;
 
 export default class PageMethods extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      rcv: true,
+      fptp: true
+    };
+  }
+
+  toggleRCV = () => {
+    this.setState(prevState => ({
+        rcv: !prevState.rcv,
+      }))
+  }
+
+  toggleFPTP = () => {
+    this.setState(prevState => ({
+        fptp: !prevState.fptp,
+      }))
+  }
+
   render() {
+    const { rcv, fptp } = this.state;
     return (
       <div css={style}>
         <Article {...content}>
             <Description>{story}</Description>
+            <Button variant="primary" onClick={this.toggleRCV}>Ranked Choice (RCV)</Button>{' '}
+            <Button variant="success" onClick={this.toggleFPTP}>First Past the Post (FPTP)</Button>{' '}
         </Article>
         <CardColumns>
-          {methods.map(method => < MethodCard info={method}/> )}
+          {methods.map(method => {
+            if (rcv && method.type === 'Ranked Choice (RCV)' 
+            || fptp && method.type === 'First Past the Post (FPTP)') {
+              return < MethodCard info={method}/> 
+            }
+            return
+          })}
         </CardColumns>
       </div>
     );
