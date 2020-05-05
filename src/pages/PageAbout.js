f3d8a95a36561cecb1cd86b64f2f4bc83dd88fa2
Yi Zhao <yizhao3721@gmail.com>
2020-05-04 22:28:45 -0400

    about and nav

--- src/pages/PageAbout.js
+++ src/pages/PageAbout.js
@@ -9,8 +9,18 @@ import Article from "../components/Article";
 const aboutProject = (
   <div>
     <p>
-    This project was created by a group of Harvard undergrads for
-    a course on campaigns and elections:
+    In the US, the predominant voting system is <b>First Past the Post (FPTP)</b>, where each voter selects one candidate and the candidate with the most votes wins. 
+    While intuitive, it creates counterproductive incentives for both candidates and voters. Groups like FairVote, Voter Choice, Ballot Ready, and Voters Choose are advocating <b>Ranked Choice Voting (RCV)</b>, where voters can rank their preferences for multiple candidates. 
+    </p>
+    <br></br>
+    <p>
+    Thus, for a course on campaigns and elections, we built <b>The Voting Methods Project</b> as a resource to help academics and election reform activists illustrate the effects of different voting systems.
+    </p>
+    <br></br>
+    <hr></hr>
+    <br></br>
+    <p>
+    This project was brought to you by:
     </p>
     <br></br>
     <ul>
@@ -50,15 +60,32 @@ const aboutStyle = css`
   div {
     font-size: 18px !important;
   }
+
+  .blurb {
+    margin-left: 8%;
+    font-size: 15px;
+    margin-bottom: 8%;
+  }
+
+  .big {
+    margin-top: 8%;
+    margin-left: 8%;
+    font-size: 50px;
+    font-weight: 700;
+  }
 `;
 
 export default class PageAbout extends Component {
   render() {
     return (
       <Article {...content}>
-        <div css={aboutStyle}>
-          <Description>{aboutProject}</Description>
+      <div css={aboutStyle}>
+        <p className = "big">Voting systems have the power to change election outcomes.</p>
+        <br></br>
+        <div className = "blurb">
+          {aboutProject}
         </div>
+      </div>
       </Article>
     );
   }
