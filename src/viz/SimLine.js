14c28a924e53678ba0cb54f6fcf3801445bcac04
Isabelle Zheng <isa.zheng@gmail.com>
2020-05-04 17:33:45 -0700

    add interactivity to simline

--- src/viz/SimLine.js
+++ src/viz/SimLine.js
@@ -4,7 +4,7 @@ import { css, jsx } from '@emotion/core';
 import { select, selectAll, mouse, event } from 'd3-selection';
 import { csv } from 'd3-fetch';
 import { path } from 'd3-path';
-import { extent, max } from 'd3-array';
+import { extent, max, bisector } from 'd3-array';
 import { scaleOrdinal, scaleLinear, scaleTime } from 'd3-scale';
 import { line } from 'd3-shape';
 import { transition } from 'd3-transition';
@@ -14,7 +14,7 @@ import { timeParse } from 'd3-time-format';
 import { axisLeft, axisBottom } from 'd3-axis';
 import d3Tip from 'd3-tip';
 
-const d3 = { select, selectAll, mouse, event, csv, path, line, extent, max, scaleOrdinal, scaleLinear, scaleTime, timeParse, transition, nest, queue, axisLeft, axisBottom };
+const d3 = { select, selectAll, mouse, event, csv, path, line, extent, max, bisector, scaleOrdinal, scaleLinear, scaleTime, timeParse, transition, nest, queue, axisLeft, axisBottom };
 
 export default class SimLine extends Component {
   constructor(props) {
@@ -78,6 +78,7 @@ export default class SimLine extends Component {
         }).sort((a, b) => a.year - b.year )
       }})
 
+      console.log(groupedData)
 
       const color = d3.scaleOrdinal()
         .domain(groups)
@@ -131,16 +132,92 @@ export default class SimLine extends Component {
       svg.selectAll('.labels')
         .data(groupedData)
         .enter()
-          .append('g')
-          .append('text')
-            .attr('class', 'labels')
-            .datum((d) => { return { name: d.name, value: d.values[d.values.length - 1]}; })
-            .attr('transform', (d) => 'translate(' + x(d.value.year) + ',' + y(d.value.value) + ')')
-            .attr('x', 10)
-            .text((d) => d.name)
-            .style('fill', (d) => color(d.name))
-            .style('font-size', '14');
+        .append('g')
+        .append('text')
+          .attr('class', 'labels')
+          .datum((d) => { return { name: d.name, value: d.values[d.values.length - 1]}; })
+          .attr('transform', (d) => 'translate(' + x(d.value.year) + ',' + y(d.value.value) + ')')
+          .attr('x', 10)
+          .text((d) => d.name)
+          .style('fill', (d) => color(d.name))
+          .style('font-size', '14');
 
+        var focus1 = svg.append("g")
+            .attr("class", "focus")
+            .style("display", "none");
+
+        focus1.append("line")
+            .attr("class", "x-hover-line1 hover-line")
+            .attr("y1", 0)
+            .attr("y2", height);
+
+        focus1.append("line")
+            .attr("class", "y-hover-line1 hover-line")
+            .attr("x1", width)
+            .attr("x2", width);
+
+        focus1.append("circle")
+            .attr("r", 7.5)
+            .attr('fill', '#9E81CC');
+
+        focus1.append("text")
+            .attr("x", 15)
+          	.attr("dy", ".31em");
+
+        var focus2 = svg.append("g")
+            .attr("class", "focus")
+            .style("display", "none");
+
+        focus2.append("line")
+            .attr("class", "x-hover-line2 hover-line")
+            .attr("y1", 0)
+            .attr("y2", height);
+
+        focus2.append("line")
+            .attr("class", "y-hover-line2 hover-line")
+            .attr("x1", width)
+            .attr("x2", width);
+
+        focus2.append("circle")
+            .attr("r", 7.5)
+            .attr('fill', '#48A36D');
+
+        focus2.append("text")
+            .attr("x", 15)
+          	.attr("dy", ".31em");
+
+
+        svg.append("rect")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+            .attr("class", "overlay")
+            .attr("width", width)
+            .attr("height", height)
+            .style('fill', 'none')
+            .style('pointer-events', 'all')
+            .on("mouseover", function() { focus1.style("display", null); focus2.style("display", null); })
+            .on("mouseout", function() { focus1.style("display", "none"); focus2.style("display", 'none');})
+            .on("mousemove", mousemove);
+
+        var bisectDate = d3.bisector(function(d) { return d.year; }).left;
+
+        function mousemove() {
+          var x0 = x.invert(d3.mouse(this)[0]),
+              i = bisectDate(groupedData[0].values, x0, 1),
+              d0 = groupedData[0].values[i - 1],
+              d1 = groupedData[0].values[i],
+              j = x0 - d0.year > d1.year - x0 ? i : i-1;
+          var a = groupedData[0].values[j];
+          var b = groupedData[1].values[j];
+          focus1.attr("transform", "translate(" + x(a.year) + "," + y(a.value) + ")");
+          focus1.select("text").text(function() { return a.value; });
+          focus1.select(".x-hover-line1").attr("y2", height - y(a.value));
+          focus1.select(".y-hover-line1").attr("x2", width + width);
+
+          focus2.attr("transform", "translate(" + x(b.year) + "," + y(b.value) + ")");
+          focus2.select("text").text(function() { return b.value; });
+          focus2.select(".x-hover-line2").attr("y2", height - y(b.value));
+          focus2.select(".y-hover-line2").attr("x2", width + width);
+        }
 
   }
 
